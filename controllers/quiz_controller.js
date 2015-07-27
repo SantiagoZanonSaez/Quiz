2241d849f89a253be9770c5a9b8f4d55633f9c34
SantiagoZanonSaez <santiagozanonsaez@gmail.com>
2015-07-27 23:45:48 +0200

    Estado definitivo tras módulo 6

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,15 @@
 exports.question=function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
-}
+	res.render('quizes/question', {pregunta: 'Capital de Italia', title: 'Pregunta'});
+};
 exports.answer=function(req, res){
 	if(req.query.respuesta==='Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
+		res.render('quizes/answer', {respuesta: 'Correcto', title: 'Respuesta'});
 	}
 	else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		res.render('quizes/answer', {respuesta: 'Incorrecto', title: 'Respuesta'});
 	}
+};
+// GET /quizes/author
+exports.author = function(req,res) {
+	res.render('/author', {title: 'Autor'});
 };
