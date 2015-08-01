c070dd026d3f0ab5872bd68dce99a9ca7a7420c8
SantiagoZanonSaez <santiagozanonsaez@gmail.com>
2015-08-01 13:36:45 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,19 @@
+var models = require ('../models/models.js');
+
 exports.question=function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia', title: 'Pregunta'});
+	//res.render('quizes/question', {pregunta: 'Capital de Italia', title: 'Pregunta'});
+	models.Quiz.findAll().then(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta, title: 'Pregunta'});
+	})
+	
 };
 exports.answer=function(req, res){
-	if(req.query.respuesta==='Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto', title: 'Respuesta'});
-	}
-	else{
-		res.render('quizes/answer', {respuesta: 'Incorrecto', title: 'Respuesta'});
-	}
-};
-// GET /quizes/author
-exports.author = function(req,res) {
-	res.render('/author', {title: 'Autor'});
+	models.Quiz.findAll().then(function(quiz){
+		if(req.query.respuesta===quiz[0].respuesta){
+			res.render('quizes/answer', {respuesta: 'Correcto', title: 'Respuesta'});
+		}
+		else{
+			res.render('quizes/answer', {respuesta: 'Incorrecto', title: 'Respuesta'});
+		}
+	})
 };
